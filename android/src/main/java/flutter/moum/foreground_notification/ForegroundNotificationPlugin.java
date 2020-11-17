30ff9d0df977c1991519c3b5d21492b34ca6c219
songyiYu <songyi0207@gmail.com>
2020-11-17 18:29:49 +0900

    Android Embedding v2

--- android/src/main/java/flutter/moum/foreground_notification/ForegroundNotificationPlugin.java
+++ android/src/main/java/flutter/moum/foreground_notification/ForegroundNotificationPlugin.java
@@ -7,8 +7,12 @@ import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import java.util.Map;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -16,21 +20,53 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** ForegroundNotificationPlugin */
-public class ForegroundNotificationPlugin implements MethodCallHandler {
+public class ForegroundNotificationPlugin implements FlutterPlugin {
   /** Plugin registration. */
 
   private static final String TAG = "ForegroundNotificationP";
 
-  static Context context;
+//  static Context context;
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.flutter.moum/foreground_notification");
-    channel.setMethodCallHandler(new ForegroundNotificationPlugin());
-    context = registrar.activeContext();
-    setNotificationChannel();
+
+    ForegroundNotificationPlugin plugin = new ForegroundNotificationPlugin();
+    plugin.setupMethodChannels(registrar.context(), registrar.messenger());
+    setNotificationChannel(registrar.context());
 
   }
 
-  private static void setNotificationChannel() {
+  private void setupMethodChannels(final Context context, BinaryMessenger messenger) {
+    final MethodChannel channel = new MethodChannel(messenger, "com.flutter.moum/foreground_notification");
+    channel.setMethodCallHandler(new MethodCallHandler() {
+      @Override
+      public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        Log.d(TAG, "onMethodCall: "+call.method);
+
+        if (call.method.equals("initialize")) {
+
+          Map<String, Object> arguments = call.arguments();
+          NotificationDetails.title = (String) arguments.get("title");
+          NotificationDetails.message = (String) arguments.get("message");
+          NotificationDetails.useChronometer = (boolean) arguments.get("useChronometer");
+          NotificationDetails.when = (long) arguments.get("when");
+
+        } else if (call.method.equals("showNotification")) {
+          Intent serviceIntent = new Intent(context, ForegroundNotificationService.class);
+          context.startService(serviceIntent);
+          result.success(true);
+
+
+        } else if (call.method.equals("closeNotification")) {
+          Intent closeIntent = new Intent(context, ForegroundNotificationService.class);
+          context.stopService(closeIntent);
+          result.success(true);
+        } else {
+          result.notImplemented();
+        }
+      }
+    });
+  }
+
+  private static void setNotificationChannel(Context context) {
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       NotificationChannel channel = new NotificationChannel("exampleChannelId", "passion", NotificationManager.IMPORTANCE_LOW);
@@ -43,31 +79,16 @@ public class ForegroundNotificationPlugin implements MethodCallHandler {
 
   }
 
+
   @Override
-  public void onMethodCall(MethodCall call, Result result) {
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    final Context context = binding.getApplicationContext();
+    setupMethodChannels(context, binding.getBinaryMessenger());
+    setNotificationChannel(context);
+  }
 
-    Log.d(TAG, "onMethodCall: "+call.method);
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
 
-    if (call.method.equals("initialize")) {
-
-      Map<String, Object> arguments = call.arguments();
-      NotificationDetails.title = (String) arguments.get("title");
-      NotificationDetails.message = (String) arguments.get("message");
-      NotificationDetails.useChronometer = (boolean) arguments.get("useChronometer");
-      NotificationDetails.when = (long) arguments.get("when");
-
-    } else if (call.method.equals("showNotification")) {
-      Intent serviceIntent = new Intent(context, ForegroundNotificationService.class);
-      context.startService(serviceIntent);
-      result.success(true);
-
-
-    } else if (call.method.equals("closeNotification")) {
-      Intent closeIntent = new Intent(context, ForegroundNotificationService.class);
-      context.stopService(closeIntent);
-      result.success(true);
-    } else {
-      result.notImplemented();
-    }
   }
 }
